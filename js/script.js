84b4c58050542c7bfc3373883ab994aff576d146
Bikcraft <felipebaiano04@gmail.com>
2023-06-16 22:05:13 -0300

    deixando codigo mais legivel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-import Slide from "./slide.js";
+import { SlideNav } from './slide.js';
 
-const slide = new Slide();
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addArrow('.prev', '.next');
+
+slide.addControl();
+
 console.log(slide);
